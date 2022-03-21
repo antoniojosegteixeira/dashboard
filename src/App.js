e36fe8462854b4a4ad64840a03d084df24c52284
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-21 17:20:01 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SideNav from "./components/SideNav";
+import { Paper, MenuItem, MenuList, Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <SideNav />
+    </Box>
+  );
+}
+
+export default App;
