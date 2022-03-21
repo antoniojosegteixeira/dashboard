a0073b1a0f32635b10d4f62c4c3e2076c1970363
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-03-21 17:55:34 -0400

    feat: create top bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./styles.css";
 
 import SideNav from "./components/SideNav";
+import TopBar from "./components/TopBar";
 import { Paper, MenuItem, MenuList, Box } from "@mui/material";
 
 function App() {
   return (
     <Box sx={{ minHeight: "100%" }}>
+      <TopBar />
       <SideNav />
     </Box>
   );
